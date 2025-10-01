f6825733c36c406cb32770a18423ff81e65759dd
Paul Toner <ptoner92@gmail.com>
2025-10-01 12:50:04 -0400

    Added module federation as a remote application

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'spotify-remote',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.jsx',
+      },
+      shared: {
+        'react': {},
+        'react-dom': {},
+        'styled-components': {},
+        // 'aws-amplify': {},
+        // '@aws-amplify/ui-react': {},
+      },
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
