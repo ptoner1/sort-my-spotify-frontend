139d595eb6c078439ee84571dccc7d03b319f4ba
Paul Toner <ptoner92@gmail.com>
2025-10-02 12:45:10 -0400

    Configure base url for loading dist/assets/

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_URL,
   plugins: [
     react(),
     federation({
